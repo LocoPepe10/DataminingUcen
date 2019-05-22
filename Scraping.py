708667aa2e1f7a4adf380e951f67f85da95db075
alfonsoopazom <30917026+alfonsoopazom@users.noreply.github.com>
2019-05-22 13:57:04 -0400

    Update Scraping.py

--- Scraping.py
+++ Scraping.py
@@ -1,1 +1,17 @@
 import scrapy
+
+class QuotesSpyder(scrapy.Spyder):
+  
+  name = 'pip'
+  
+  def start_requests(self):
+    url=[]
+    for url in urls:
+      yield scrapy.Request(url=url, callback=self.parse)
+      
+  def parse(self, response):
+    pagina = response.url.split("/")[-2]
+    filename = 'pip-%s.html' % page 
+    with open (filename,'wb') as f:
+      f.write(response.body)
+    self.log('Archivo Creado %s' % fileame)
