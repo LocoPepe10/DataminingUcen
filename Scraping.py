b237fbe9abde63ec2b38cb9eeaa726e2773b94a4
alfonsoopazom <30917026+alfonsoopazom@users.noreply.github.com>
2019-05-22 14:03:51 -0400

    Update Scraping.py

--- Scraping.py
+++ Scraping.py
@@ -9,9 +9,10 @@ class QuotesSpyder(scrapy.Spyder):
     for url in urls:
       yield scrapy.Request(url=url, callback=self.parse)
       
-  def parse(self, response):
+  def parse(self, response): #Analisis de gramatica 
     pagina = response.url.split("/")[-2]
     filename = 'pip-%s.html' % page 
+    
     with open (filename,'wb') as f:
       f.write(response.body)
     self.log('Archivo Creado %s' % fileame)
